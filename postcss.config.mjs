bc6ae7b17c5d17852246e7d1de21c94347c0b989
Adil0710 <padil2246@gmail.com>
2025-04-10 19:53:13 +0530

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.ts (or .js)
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
